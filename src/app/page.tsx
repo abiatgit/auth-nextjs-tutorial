610296ea91f422149b8104e27a033acef82eba83
Abi George <contactabisgeorge@gmail.com>
2025-05-28 11:39:52 +0100

    setup authjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import { SignOut } from "@/components/sign-out";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 const Page = async () => {
+  const section =await auth()
+  if(!section)redirect("/sign-up")
   return (
     <>
       <div className="bg-gray-100 rounded-lg p-4 text-center mb-6">
